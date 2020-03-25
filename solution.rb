7e7e861f01404b3a3e41b45642d7c922801013e3
caicindy87 <caicindy87@gmail.com>
2020-03-24 22:30:18 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -21,7 +21,6 @@ class Deck
                  @cards << Card.new(suit, rank)
              end
         end
-      
     end
 
     def choose_card
@@ -31,24 +30,3 @@ end
 
 
     
-
-
-
-
-#Card.new("A", "Hearts")
-# binding.pry
-# puts 's'
-
-# Construct a class for a Deck of cards, which has an attribute cards consisting of an array of 52 Card objects.
-# A Card consists of a rank ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") paired with a suit ("Hearts", "Clubs", "Diamonds", "Spades")
-# Your Deck should have a method #choose_card which selects a random card from the deck and removes it.
-
-
-
-
-
-# ranks.each do |rank|
-#     suits.each do |suit|
-#          @cards << Card.new(suit: suit, rank: rank)
-#      end
-# end
