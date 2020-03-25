7c169e1f2412e6bab9dc0420a3c258df59513fdd
caicindy87 <caicindy87@gmail.com>
2020-03-24 22:30:04 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,37 +1,54 @@
 require 'pry'
+
+class Card
+    attr_accessor :suit, :rank
+
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
+end
+
 class Deck
     attr_accessor :cards
 
     def initialize
-        @cards = Card.all
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        @cards = []
+        suits.each do |suit|
+            ranks.each do |rank|
+                 @cards << Card.new(suit, rank)
+             end
+        end
+      
     end
 
-    def create_deck(rank:, suit:)
-        Card.new(rank: "A", suit: "Hearts")
-    end
-        rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-        suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
-end
-
-class Card
-    attr_accessor :rank, :suit
-    @@all = []
-
-    def initialize(rank:, suit:)
-        @rank = rank
-        @suit = suit
-        @@all << self
-    end
-
-    def self.all
-        @@all
+    def choose_card
+        @cards.pop
     end
 end
 
-binding.pry
-puts 's'
 
+    
+
+
+
+
+#Card.new("A", "Hearts")
+# binding.pry
+# puts 's'
 
 # Construct a class for a Deck of cards, which has an attribute cards consisting of an array of 52 Card objects.
 # A Card consists of a rank ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K") paired with a suit ("Hearts", "Clubs", "Diamonds", "Spades")
 # Your Deck should have a method #choose_card which selects a random card from the deck and removes it.
+
+
+
+
+
+# ranks.each do |rank|
+#     suits.each do |suit|
+#          @cards << Card.new(suit: suit, rank: rank)
+#      end
+# end
